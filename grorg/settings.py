b3b6fac7c18cccf7370661b8ff6b2ec610c37ab7
Andrew Godwin <andrew@aeracode.org>
2016-01-12 21:01:22 -0800

    Deployment fixes

--- grorg/settings.py
+++ grorg/settings.py
@@ -83,6 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
