a24c9c8a4e75a98e5f3a2fe203472b209ed8815f
Andrew Godwin <andrew@aeracode.org>
2019-12-17 12:56:11 -0700

    Add whitenoise

--- grorg/settings.py
+++ grorg/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
