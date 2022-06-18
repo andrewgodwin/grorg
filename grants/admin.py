27618f49006aed1c0066d6ec40a07506223d3ea0
Jeff Triplett <jeff.triplett@gmail.com>
2022-06-18 12:42:29 -0500

    :pencil: Fills in missing model admins

--- grants/admin.py
+++ grants/admin.py
@@ -2,29 +2,53 @@ from __future__ import annotations
 
 from django.contrib import admin
 
-from .models import Answer, Applicant, Program, Score
+from grants import models
 
 
 class AnswerInline(admin.StackedInline):
-    model = Answer
+    model = models.Answer
     extra = 1
 
 
-admin.site.register(
-    Program,
-    list_display=["id", "name", "completed"],
-    list_display_links=["id", "name"],
-    prepopulated_fields={"slug": ("name",)},
-)
+@admin.register(models.Allocation)
+class AllocationAdmin(admin.ModelAdmin):
+    pass
 
-admin.site.register(
-    Applicant,
-    list_display=["id", "name", "email", "program", "applied"],
-    list_display_links=["id", "name"],
-    inlines=[AnswerInline],
-)
 
-admin.site.register(
-    Score,
-    list_display=["id", "applicant", "user"],
-)
+@admin.register(models.Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Applicant)
+class ApplicantAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "email", "program", "applied"]
+    list_display_links = ["id", "name"]
+    inlines = [AnswerInline]
+
+
+@admin.register(models.Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "completed"]
+    list_display_links = ["id", "name"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Score)
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ["id", "applicant", "user"]
+
+
+@admin.register(models.UploadedCSV)
+class UploadedCSVAdmin(admin.ModelAdmin):
+    pass
