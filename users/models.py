268cd2c324033d21a969c5014c4bd1982249617a
Jeff Triplett <jeff.triplett@gmail.com>
2022-06-18 11:44:21 -0500

    :tractor: Upgrades models __unicode__ => __str__

--- users/models.py
+++ users/models.py
@@ -65,5 +65,5 @@ class User(AbstractBaseUser, PermissionsMixin):
         """
         send_mail(subject, message, from_email, [self.email], **kwargs)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name or self.email
